154a4d43d13448fb66c30b7ab493d4da6b993583
City <City@ASUS>
2021-08-23 21:18:24 +0300

    Добавьте файлы проекта.

---
+++ ctcontrol/Model/MainModel.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Windows.Forms;
+using System.ComponentModel;
+
+namespace ctcontrol.Model
+{
+    public class MainModel
+    {
+
+        #region Methods
+
+        public string SetDate()
+        {
+            DateTime now = DateTime.Now;
+            return string.Format(now.ToString("dd. ") + "{0:Y}", now);
+        }
+
+        public string SetTime()
+        {
+            return DateTime.Now.ToString("HH:mm");
+        }
+        public string SetSecond()
+        {
+            return DateTime.Now.ToString("ss");
+        }
+        public string SetDay()
+        {
+            char[] day = DateTime.Now.ToString("dddd").ToCharArray();
+            day[0] = char.ToUpper(day[0]);
+            string Day = new string(day);
+            return Day;
+        }
+        public string SetWorkTime()
+        {
+            return "00T 00h 00m 00s";
+        }
+        public string SetBatary()
+        {
+            return (SystemInformation.PowerStatus.BatteryLifePercent * 100).ToString() + "%";
+        }
+
+        #endregion
+
+    }
+}
---
+++ ctcontrol/ViewModel/MainViewModel.cs
@@ -0,0 +1,128 @@
+﻿using ctcontrol.Model;
+using System;
+using System.ComponentModel;
+
+namespace ctcontrol.ViewModel
+{
+    class MainViewModel : INotifyPropertyChanged
+    {
+        readonly MainModel mm = new MainModel();
+        private readonly System.Windows.Forms.Timer _timer = new System.Windows.Forms.Timer();
+
+        #region Data
+        private string _data;
+        private string _time;
+        private string _second;
+        private string _day;
+        private string _worktime;
+        private string _batary;
+
+        public string Data
+        {
+            get
+            {
+                return _data;
+            }
+            set
+            {
+                _data = value;
+                OnPropertyChanged("Data");
+            }
+        }
+        public string Time
+        {
+            get
+            {
+                return _time;
+            }
+            set
+            {
+                _time = value;
+                OnPropertyChanged("Time");
+            }
+        }
+        public string Second
+        {
+            get
+            {
+                return _second;
+            }
+            set
+            {
+                _second = value;
+                OnPropertyChanged("Second");
+            }
+        }
+        public string Day
+        {
+            get
+            {
+                return _day;
+            }
+            set
+            {
+                _day = value;
+                OnPropertyChanged("Day");
+            }
+        }
+
+        public string WorkTime
+        {
+            get
+            {
+                return _worktime;
+            }
+            set
+            {
+                _worktime = value;
+                OnPropertyChanged("WorkTime");
+            }
+        }
+
+        public string Batary
+        {
+            get
+            {
+                return _batary;
+            }
+            set
+            {
+                _batary = value;
+                OnPropertyChanged("Batary");
+            }
+        }
+
+        #endregion
+
+        #region Inteface
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        #endregion
+
+        public MainViewModel()
+        {
+            StartClock();
+        }
+
+        private void StartClock()
+        {
+            _timer.Enabled = true;
+            _timer.Tick += Timer_Tick;
+            _timer.Start();
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            Data = mm.SetDate();
+            Time = mm.SetTime();
+            Second = mm.SetSecond();
+            Day = mm.SetDay();
+            WorkTime = mm.SetWorkTime();
+            Batary = mm.SetBatary();
+        }
+
+    }
+}
