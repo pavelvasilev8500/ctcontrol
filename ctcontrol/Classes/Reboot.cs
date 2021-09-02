d26327a8adf0cf3b3556d6e0f479bb5a0a325f7e
pavelvasilev8500 <61418223+pavelvasilev8500@users.noreply.github.com>
2021-09-02 22:14:29 +0300

    Update Reboot.cs

--- ctcontrol/Classes/Reboot.cs
+++ ctcontrol/Classes/Reboot.cs
@@ -5,29 +5,28 @@ namespace ctcontrol.Model
 {
     class Reboot
     {
-        //импортируем API функцию InitiateSystemShutdown
         [DllImport("advapi32.dll", EntryPoint = "InitiateSystemShutdownEx")]
         static extern int InitiateSystemShutdown(string lpMachineName, string lpMessage, int dwTimeout, bool bForceAppsClosed, bool bRebootAfterShutdown);
-        //импортируем API функцию AdjustTokenPrivileges
+        
         [DllImport("advapi32.dll", ExactSpelling = true, SetLastError = true)]
         internal static extern bool AdjustTokenPrivileges(IntPtr htok, bool disall,
         ref TokPriv1Luid newst, int len, IntPtr prev, IntPtr relen);
-        //импортируем API функцию GetCurrentProcess
+        
         [DllImport("kernel32.dll", ExactSpelling = true)]
         internal static extern IntPtr GetCurrentProcess();
-        //импортируем API функцию OpenProcessToken
+        
         [DllImport("advapi32.dll", ExactSpelling = true, SetLastError = true)]
         internal static extern bool OpenProcessToken(IntPtr h, int acc, ref IntPtr phtok);
-        //импортируем API функцию LookupPrivilegeValue
+        
         [DllImport("advapi32.dll", SetLastError = true)]
         internal static extern bool LookupPrivilegeValue(string host, string name, ref long pluid);
-        //импортируем API функцию LockWorkStation
+        
         [DllImport("user32.dll", EntryPoint = "LockWorkStation")]
         static extern bool LockWorkStation();
-        //импортируем API функцию SetSuspendState
+        
         [DllImport("Powrprof.dll", SetLastError = true)]
         static extern uint SetSuspendState(bool hibernate, bool forceCritical, bool disableWakeEvent);
-        //объявляем структуру TokPriv1Luid для работы с привилегиями
+        
         [StructLayout(LayoutKind.Sequential, Pack = 1)]
         internal struct TokPriv1Luid
         {
@@ -35,37 +34,32 @@ namespace ctcontrol.Model
             public long Luid;
             public int Attr;
         }
-        //объявляем необходимые, для API функций, константые значения, согласно MSDN
+        
         internal const int SE_PRIVILEGE_ENABLED = 0x00000002;
         internal const int TOKEN_QUERY = 0x00000008;
         internal const int TOKEN_ADJUST_PRIVILEGES = 0x00000020;
         internal const string SE_SHUTDOWN_NAME = "SeShutdownPrivilege";
-        //функция SetPriv для повышения привилегий процесса
+        
         private void SetPriv()
         {
-            TokPriv1Luid tkp; //экземпляр структуры TokPriv1Luid 
+            TokPriv1Luid tkp;
             IntPtr htok = IntPtr.Zero;
-            //открываем "интерфейс" доступа для своего процесса
             if (OpenProcessToken(GetCurrentProcess(), TOKEN_ADJUST_PRIVILEGES | TOKEN_QUERY, ref htok))
             {
-                //заполняем поля структуры
                 tkp.Count = 1;
                 tkp.Attr = SE_PRIVILEGE_ENABLED;
                 tkp.Luid = 0;
-                //получаем системный идентификатор необходимой нам привилегии
                 LookupPrivilegeValue(null, SE_SHUTDOWN_NAME, ref tkp.Luid);
-                //повышем привилигеию своему процессу
                 AdjustTokenPrivileges(htok, false, ref tkp, 0, IntPtr.Zero, IntPtr.Zero);
             }
         }
-        //публичный метод для перезагрузки/выключения машины
+        
         public int halt(bool RSh, bool Force)
         {
-            SetPriv(); //получаем привилегия
-            //вызываем функцию InitiateSystemShutdown, передавая ей необходимые параметры
+            SetPriv();
             return InitiateSystemShutdown(null, null, 0, Force, RSh);
         }
-        //публичный метод для блокировки операционной системы
+        
         public int Lock()
         {
             if (LockWorkStation())
