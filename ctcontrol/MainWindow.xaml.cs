116ea7c8757c4cf45f69fbc3c218ffa557cf71b1
Aliaksei Valatouski <abvalatouski@gmail.com>
2021-09-02 23:42:37 +0300

    Merge `Position` class into `MainWindow`.

--- ctcontrol/MainWindow.xaml.cs
+++ ctcontrol/MainWindow.xaml.cs
@@ -1,39 +1,33 @@
-﻿using ctcontrol.Classes;
-using Microsoft.Win32;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace ctcontrol
 {
-    /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
-            this.Left = Position.LeftPosition();
-            this.Top = Position.TopPosition();
-            //Chanege propery if need app icon.
-            this.ShowInTaskbar = false;
+            Left = GetLeft();
+            Top = GetTop();
+            ShowInTaskbar = false;
         }
 
         public void Move(object sender, EventArgs e)
         {
-            this.DragMove();
+            DragMove();
+        }
+
+        private static double GetLeft()
+        {
+            const int OffsetFromScreenBorder = 200;
+            return SystemParameters.WorkArea.Right - OffsetFromScreenBorder;
+        }
+
+        private static double GetTop()
+        {
+            const int OffsetFromScreenBorder = 20;
+            return SystemParameters.WorkArea.Top + OffsetFromScreenBorder;
         }
     }
 }
