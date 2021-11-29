02230f603597d6943333e798fe95ca9ee9b0f089
павел васильев <pavelvasilev@live.com>
2021-11-29 12:48:51 +0300

    Bug fix

--- ControlLibrary/Properties/Settings.Designer.cs
+++ ControlLibrary/Properties/Settings.Designer.cs
@@ -25,7 +25,7 @@ namespace ControlLibrary.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("1")]
+        [global::System.Configuration.DefaultSettingValueAttribute("0")]
         public string PCID {
             get {
                 return ((string)(this["PCID"]));
